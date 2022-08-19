443c6814fb3346e5c8ceba37ddc21aefaf92396c
Kristián Kunc <kristian.kunc@gmail.com>
2022-08-19 12:05:16 +0200

    :sparkles: add cursor wait style for loading

--- static/script.js
+++ static/script.js
@@ -2,10 +2,16 @@ const nextBtn = document.getElementById("next-btn");
 const spotifyEmbed = document.getElementById("spotify-embed");
 
 nextBtn.addEventListener("click", function() {
+    nextBtn.style.cursor = "wait";
+    document.body.style.cursor = "wait";
+
     fetch("/api/track")
         .then(res => res.json())
         .then(data => {
             spotifyEmbed.src = `https://open.spotify.com/embed/track/${data.trackId}`;
+
+            nextBtn.style.cursor = "pointer";
+            document.body.style.cursor = "default";
         }
     );
 });
